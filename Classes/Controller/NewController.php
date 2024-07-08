6c53bdbfa6b29cc268e194bd2a1df2dded27e0ea
Michael Lang <7070587+t3md@users.noreply.github.com>
2024-07-08 10:21:46 +0200

    Add files via upload

---
+++ Classes/Controller/NewController.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace In2code\Femanagerextended\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Error\Http\UnauthorizedException;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+
+class NewRecordController extends AbstractFrontendController
+{
+
+}
